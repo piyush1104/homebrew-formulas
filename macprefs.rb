44a7947333abbc576b740bce92f86cbc284df794
Piyush Bansal <19729997+piyush1104@users.noreply.github.com>
2021-11-08 18:48:27 +0530

    Create macprefs.rb

---
+++ macprefs.rb
@@ -0,0 +1,18 @@
+class Macprefs < Formula
+    include Language::Python::Virtualenv
+  
+    desc "Backup and Restore your Mac System and App Preferences"
+    homepage "https://github.com/piyush1104/macprefs"
+    url "https://github.com/clintmod/macprefs/archive/v1.0.26.tar.gz"
+    sha256 "03fcd5bd778d53fa9b89ec960b2fab18e5a8303ab15cbd79125def4cc9af8228"
+  
+    depends_on :python if MacOS.version <= :snow_leopard
+  
+    def install
+      bin.install Dir["*"]
+    end
+  
+    test do
+      system "#{bin}/macprefs", "--help"
+    end
+  end
