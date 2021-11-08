65fee8abe88bb94213c01da7b8c0f4669b737bde
Piyush Bansal <bansalpiyush177@gmail.com>
2021-11-08 19:10:07 +0530

    Updating to version v1.0.27

--- macprefs.rb
+++ macprefs.rb
@@ -3,10 +3,10 @@ class Macprefs < Formula
   
     desc "Backup and Restore your Mac System and App Preferences"
     homepage "https://github.com/piyush1104/macprefs"
-    url "https://github.com/clintmod/macprefs/archive/v1.0.26.tar.gz"
-    sha256 "03fcd5bd778d53fa9b89ec960b2fab18e5a8303ab15cbd79125def4cc9af8228"
+    url "https://github.com/piyush1104/macprefs/archive/v1.0.27.tar.gz"
+    sha256 "248fa4c9ef4ddb91b31f936576472afce4eacdce71ae2bab18f166c7bd739aa1"
   
-    depends_on :python if MacOS.version <= :snow_leopard
+    depends_on :python3
   
     def install
       bin.install Dir["*"]
@@ -16,3 +16,4 @@ class Macprefs < Formula
       system "#{bin}/macprefs", "--help"
     end
   end
+  
