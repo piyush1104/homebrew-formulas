22a341ea71ae6eeee2122c038eeadbec66f591e9
Piyush Bansal <19729997+piyush1104@users.noreply.github.com>
2021-11-09 14:58:29 +0530

    Update macprefs.rb

--- macprefs.rb
+++ macprefs.rb
@@ -3,8 +3,8 @@ class Macprefs < Formula
   
     desc "Backup and Restore your Mac System and App Preferences"
     homepage "https://github.com/piyush1104/macprefs"
-    url "https://github.com/piyush1104/macprefs/archive/v1.0.27.tar.gz"
-    sha256 "248fa4c9ef4ddb91b31f936576472afce4eacdce71ae2bab18f166c7bd739aa1"
+    url "https://github.com/piyush1104/macprefs/archive/v1.0.28.tar.gz"
+    sha256 "5d37a1c998c7982a82c449c105037ef2defa5b840f1b67a0a7bd1a6e1b1279e9"
   
     depends_on :python if MacOS.version <= :snow_leopard
   
